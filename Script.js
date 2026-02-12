986ad4b456c41498f2d17fdeb725f78d78de3c20
manekemu-oss <manekemu@gmail.com>
2026-02-12 22:19:44 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,17 @@
+const sections = document.querySelectorAll('.moment');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = "1";
+            entry.target.style.transform = "translateY(0)";
+        }
+    });
+}, { threshold: 0.1 });
+
+sections.forEach(section => {
+    section.style.opacity = "0";
+    section.style.transform = "translateY(20px)";
+    section.style.transition = "all 1s ease-out";
+    observer.observe(section);
+});
